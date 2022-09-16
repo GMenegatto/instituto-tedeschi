a9fd43d87574a8adde8c7557f151537ea8e41a86
Menegatto <dev1@destrasoftware.com.br>
2022-09-16 13:07:59 -0300

    video

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+var botao = document.getElementById("button-modal1")
+var fundo = document.getElementById("modal")
+botao.onclick = function() {
+    var botao = document.getElementById("body");
+    botao.disabled = false;
+    botao.style.overflow = "hidden";
+    var botao = document.getElementById("modal");
+    botao.disabled = false;
+    botao.style.display = "flex";
+
+
+}
+
+
+
+var botao = document.getElementById("close-modal")
+botao.onclick = function() {
+    var botao = document.getElementById("body");
+    botao.disabled = false;
+    botao.style.overflow = "scroll";
+    var botao = document.getElementById("modal");
+    botao.disabled = false;
+    botao.style.display = "none";
+
+}
+
+ 
+fundo.onclick = function() {
+    var botao = document.getElementById("body");
+    botao.disabled = false;
+    botao.style.overflow = "scroll";
+    var botao = document.getElementById("modal");
+    botao.disabled = false;
+    botao.style.display = "none";
+
+ }
+
+
+fundo.onclick = function() {
+    var botao = document.getElementById("body");
+    botao.disabled = false;
+    botao.style.overflow = "scroll";
+    var botao = document.getElementById("modal2");
+    botao.disabled = false;
+    botao.style.display = "none";
+
+}
